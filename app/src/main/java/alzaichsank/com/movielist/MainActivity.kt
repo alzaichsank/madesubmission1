029b1aae98da48d038c4a945a43d85834a2c41d4
Alza Devloper <alzaichsank@gmail.com>
2019-06-24 10:20:43 +0700

    Init Project

---
+++ app/src/main/java/alzaichsank/com/movielist/MainActivity.kt
@@ -0,0 +1,12 @@
+package alzaichsank.com.movielist
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
